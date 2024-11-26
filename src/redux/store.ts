b150eabb65ba4eee47e9eac6d57a13f16d9193e0
sanjeev0303 <sanjeev.081021@gmail.com>
2024-11-26 10:03:38 +0530

    initial project setup

---
+++ src/redux/store.ts
@@ -0,0 +1,26 @@
+'use  client'
+
+import { combineReducers, configureStore } from "@reduxjs/toolkit"
+import { TypedUseSelectorHook, useSelector } from "react-redux"
+
+
+const rootReducer = combineReducers({
+    // Add your reducers here
+})
+
+
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+})
+
+// exporting the type definitions
+export type RootState = ReturnType <typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+
+//this useAppSelector has type definitions added
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
