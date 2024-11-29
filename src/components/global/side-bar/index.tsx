445da640277a4c3add26d4416e9607c7c3a18e41
sanjeev0303 <sanjeev.081021@gmail.com>
2024-11-30 04:23:38 +0530

    adding sidebar

--- src/components/global/side-bar/index.tsx
+++ src/components/global/side-bar/index.tsx
@@ -1,9 +1,16 @@
-"use client"
+"use client";
 
 import { useGroupChatOnline } from "@/hooks/groups";
 import { useSidebar } from "@/hooks/navigation";
 import { cn } from "@/lib/utils";
 import React from "react";
+import DropDown from "../drop-down";
+import Image from "next/image";
+import { CarotSort } from "@/icons";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Group, Plus } from "lucide-react";
+import { v4 as uuidV4 } from "uuid"
 
 type SideBarProps = {
   groupid: string;
@@ -64,7 +71,82 @@ const SideBar = ({ groupid, userid, mobile }: SideBarProps) => {
         !mobile ? "hidden bg-black md:w-[300px] fixed md:flex" : "w-full flex"
       )}
     >
-        adsfadf
+      {groups.groups && groups.groups.length > 0 && (
+        <DropDown
+          title="Groups"
+          trigger={
+            <div className="w-full flex items-center justify-between text-themeTextGray md:border-[1px] border-themeGray p-3 rounded-xl">
+              <div className="flex gap-x-3 items-center">
+                <img
+                  src={`https://ucarecdn.com/${
+                    groupInfo.group?.icon as string
+                  }`}
+                  alt="icon"
+                  className="w-10 rounded-lg"
+                />
+                <p className="text-sm">{groupInfo.group?.name}</p>
+              </div>
+              <span className="">
+                <CarotSort />
+              </span>
+            </div>
+          }
+        >
+          {groups &&
+            groups.groups.length > 0 &&
+            groups.groups.map(
+              (item) =>
+                item.id !== groupid && (
+                  <Link
+                  key={item.id}
+                    href={`/group/${item.id}/channel/${channels?.channels?.[0]
+                      .id!}`}
+                  >
+                    <Button
+                      variant="ghost"
+                      className="flex gap-2 w-full justify-start hover:bg-themeGray items-center"
+                    >
+                      <Group />
+                      {item.name}
+                    </Button>
+                  </Link>
+                )
+            )}
+        </DropDown>
+      )}
+
+      <div className="flex flex-col gap-y-5">
+        <div className="flex justify-between items-center">
+            <p className="text-xs text-[#F7ECE9]">CHANNELS</p>
+            {
+                userid === groupInfo.group?.userId && (
+                    <Plus
+                    size={16}
+                    className={cn("text-themeTextGray cursor-pointer", isPending && "opacity-70")}
+                    {...(!isPending && {
+                        onClick: () =>
+                        mutate({
+                            id: uuidV4(),
+                            icon: "general",
+                            name: "unnamed",
+                            createdAt: new Date(),
+                            groupId: groupid
+                        })
+                    })}
+                    />
+                )
+            }
+        </div>
+
+        <SideBarMenu
+        channels={channels?.channels}
+        optimisticChannel={variables}
+        loading={isPending}
+        groupid={groupid}
+        groupUserId={groupInfo.group?.userId}
+        userId={userid}
+        />
+      </div>
     </div>
   );
 };
