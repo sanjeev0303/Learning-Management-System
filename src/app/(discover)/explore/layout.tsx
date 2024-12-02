a7ac12a5cfc519f301005cee70c3739ae02c09a0
sanjeev0303 <sanjeev.081021@gmail.com>
2024-12-02 19:06:02 +0530

    adding explore-loading

--- src/app/(discover)/explore/layout.tsx
+++ src/app/(discover)/explore/layout.tsx
@@ -1,5 +1,6 @@
-import Navbar from '@/app/group/_components/navbar'
+// import Navbar from '@/app/group/_components/navbar'
 import React from 'react'
+import { Navbar } from './_components/navbar'
 
 type DiscoverLayoutProps = {
     children: React.ReactNode
@@ -8,6 +9,7 @@ type DiscoverLayoutProps = {
 const DiscoverLayout = ({ children }: DiscoverLayoutProps) => {
   return (
     <div className='flex flex-col min-h-screen bg-balck pb-10'>
+        <Navbar />
         {children}
     </div>
   )
