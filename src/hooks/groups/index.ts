9cd6eb1319c0f50ffa1111524a5ce9211bd9ed50
sanjeev0303 <sanjeev.081021@gmail.com>
2024-12-11 20:45:31 +0530

    adding gallery-modal

--- src/hooks/groups/index.ts
+++ src/hooks/groups/index.ts
@@ -295,167 +295,158 @@ export const useGroupInfo = () => {
 };
 
 export const useGroupAbout = (
-  description: string | null,
-  jsonDescription: string | null,
-  htmlDescription: string | null,
-  currentMedia: string,
-  groupid: string
-) => {
-  const editor = useRef<HTMLFormElement | null>(null);
-
-  const mediaType = validateURLString(currentMedia);
-
-  const [activeMedia, setActiveMedia] = useState<{
-    url: string | undefined;
-    type: string;
-  }>(
-    mediaType.type === "IMAGE"
-      ? {
-          url: currentMedia,
-          type: mediaType.type,
+    description: string | null,
+    jsonDescription: string | null,
+    htmlDescription: string | null,
+    currentMedia: string,
+    groupid: string,
+  ) => {
+    const editor = useRef<HTMLFormElement | null>(null)
+    const mediaType = validateURLString(currentMedia)
+    const [activeMedia, setActiveMedia] = useState<
+      | {
+          url: string | undefined
+          type: string
         }
-      : { ...mediaType }
-  );
+      | undefined
+    >(
+      mediaType.type === "IMAGE"
+        ? {
+            url: currentMedia,
+            type: mediaType.type,
+          }
+        : { ...mediaType },
+    )
 
-  const jsonContent =
-    jsonDescription !== null
-      ? JSON.parse(jsonDescription as string)
-      : undefined;
+    const jsonContent =
+      jsonDescription !== null ? JSON.parse(jsonDescription as string) : undefined
 
-  const [onJsonDescription, setOnJsonDescription] = useState<
-    JSONContent | undefined
-  >(jsonContent);
+    const [onJsonDescription, setJsonDescription] = useState<
+      JSONContent | undefined
+    >(jsonContent)
 
-  const [onDescription, setOnDescription] = useState<string | undefined>(
-    description || undefined
-  );
+    const [onDescription, setOnDescription] = useState<string | undefined>(
+      description || undefined,
+    )
 
-  const [onHtmlDescription, setOnHtmlDescription] = useState<
-    string | undefined
-  >(htmlDescription || undefined);
+    const [onHtmlDescription, setOnHtmlDescription] = useState<
+      string | undefined
+    >(htmlDescription || undefined)
 
-  const [onEditDescription, setOnEditDescription] = useState<boolean>(false);
+    const [onEditDescription, setOnEditDescription] = useState<boolean>(false)
 
-  const {
-    setValue,
-    formState: { errors },
-    handleSubmit,
-  } = useForm<z.infer<typeof GroupSettingsSchema>>({
-    resolver: zodResolver(GroupSettingsSchema)
-  })
+    const {
+      setValue,
+      formState: { errors },
+      handleSubmit,
+    } = useForm<z.infer<typeof GroupSettingsSchema>>({
+      resolver: zodResolver(GroupSettingsSchema),
+    })
 
-  const onSetDescriptions = () => {
-    const JsonContent = JSON.stringify(onJsonDescription)
-    setValue("jsondescription", jsonContent)
-    setValue("description", onDescription)
-    setValue("htmldescription", onHtmlDescription)
-  }
+    const onSetDescriptions = () => {
+      const JsonContent = JSON.stringify(onJsonDescription)
+      setValue("jsondescription", JsonContent)
+      setValue("description", onDescription)
+      setValue("htmldescription", onHtmlDescription)
+    }
 
-  useEffect(() => {
+    useEffect(() => {
+      onSetDescriptions()
+      return () => {
+        onSetDescriptions()
+      }
+    }, [onJsonDescription, onDescription])
 
-    onSetDescriptions()
-    return () => onSetDescriptions()
-
-  }, [onJsonDescription, onDescription])
-
-  const onEditTextEditor = (event: Event) => {
-    if(editor.current) {
+    const onEditTextEditor = (event: Event) => {
+      if (editor.current) {
         !editor.current.contains(event.target as Node | null)
-         ? setOnEditDescription(false)
+          ? setOnEditDescription(false)
           : setOnEditDescription(true)
+      }
     }
-  }
 
-  useEffect(() => {
-
-    document.addEventListener("click", onEditTextEditor, false)
-    return () => {
+    useEffect(() => {
+      document.addEventListener("click", onEditTextEditor, false)
+      return () => {
         document.removeEventListener("click", onEditTextEditor, false)
-    }
+      }
+    }, [])
 
-  }, [])
-
-  const { mutate, isPending } = useMutation({
-    mutationKey: ["about-description"],
-    mutationFn: async (values: z.infer<typeof GroupSettingsSchema>) => {
+    const { mutate, isPending } = useMutation({
+      mutationKey: ["about-description"],
+      mutationFn: async (values: z.infer<typeof GroupSettingsSchema>) => {
         if (values.description) {
-            const update = await onUpDateGroupSettings(
-                groupid,
-                "DESCRIPTION",
-                values.description,
-                `/about/${groupid}`
-            )
-
-            if (update.status !== 200) {
-                return toast("Error", {
-                    description: "Oops! looks like your form is empty"
-                })
-            }
-        }
-
-        if (values.jsondescription) {
-            const update  = await onUpDateGroupSettings(
-                groupid,
-                "JSONDESCRIPTION",
-                values.jsondescription,
-                `/about/${groupid}`
-            )
-
-            if (update.status !== 200) {
-                return toast("Error", {
-                    description: "!Oops looks like your form is empty"
-                })
-            }
-        }
-
-        if (values.htmldescription) {
-            const update  = await onUpDateGroupSettings(
-                groupid,
-                "HTMLDESCRIPTION",
-                values.htmldescription,
-                `/about/${groupid}`
-            )
-
-            if (update.status !== 200) {
-                return toast("Error", {
-                    description: "!Oops looks like your form is empty"
-                })
-            }
-        }
-
-        if (
-            !values.description &&
-            !values.jsondescription &&
-            !values.htmldescription
-        ) {
-            return toast("Success", {
-                description: "Group description updated"
+          const updated = await onUpDateGroupSettings(
+            groupid,
+            "DESCRIPTION",
+            values.description,
+            `/about/${groupid}`,
+          )
+          if (updated.status !== 200) {
+            return toast("Error", {
+              description: "Oops! looks like your form is empty",
             })
+          }
         }
-    },
-  })
+        if (values.jsondescription) {
+          const updated = await onUpDateGroupSettings(
+            groupid,
+            "JSONDESCRIPTION",
+            values.jsondescription,
+            `/about/${groupid}`,
+          )
+          if (updated.status !== 200) {
+            return toast("Error", {
+              description: "Oops! looks like your form is empty",
+            })
+          }
+        }
+        if (values.htmldescription) {
+          const updated = await onUpDateGroupSettings(
+            groupid,
+            "HTMLDESCRIPTION",
+            values.htmldescription,
+            `/about/${groupid}`,
+          )
+          if (updated.status !== 200) {
+            return toast("Error", {
+              description: "Oops! looks like your form is empty",
+            })
+          }
+        }
+        if (
+          !values.description &&
+          !values.jsondescription &&
+          !values.htmldescription
+        ) {
+          return toast("Error", {
+            description: "Oops! looks like your form is empty",
+          })
+        }
+        return toast("Success", {
+          description: "Group description updated",
+        })
+      },
+    })
+    const onSetActiveMedia = (media: { url: string | undefined; type: string }) =>
+      setActiveMedia(media)
 
-  const onSetActionMedia = ( media: {
-    url: string | undefined
-    type: string
-  } ) => setActiveMedia(media)
+    const onUpdateDescription = handleSubmit(async (values) => {
+      mutate(values)
+    })
 
-  const onUpdateDescription = handleSubmit(async(values) => {
-    mutate(values)
-  })
-
-  return {
-    setOnDescription,
-    onDescription,
-    setOnJsonDescription,
-    onJsonDescription,
-    errors,
-    onEditDescription,
-    editor,
-    activeMedia,
-    onSetActionMedia,
-    onUpdateDescription,
-    isPending
+    return {
+      setOnDescription,
+      onDescription,
+      setJsonDescription,
+      onJsonDescription,
+      errors,
+      onEditDescription,
+      editor,
+      activeMedia,
+      onSetActiveMedia,
+      setOnHtmlDescription,
+      onUpdateDescription,
+      isPending,
+    }
   }
-
-};
