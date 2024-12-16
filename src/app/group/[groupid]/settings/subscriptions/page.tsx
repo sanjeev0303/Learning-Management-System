58d0015a6a32dc4fee2b98d70e9d404673202cad
sanjeev0303 <sanjeev.081021@gmail.com>
2024-12-16 18:13:53 +0530

    adding subscription form

--- src/app/group/[groupid]/settings/subscriptions/page.tsx
+++ src/app/group/[groupid]/settings/subscriptions/page.tsx
@@ -10,7 +10,7 @@ const SubscriptionPage = ({ params }: SubscriptionProps) => {
     <div className='p-10 flex flex-col gap-y-10'>
         <h2 className='font-bold text-3xl'> Group Subscriptions</h2>
         <div className='grid grid-cols-1 lg:grid-cols-5 gap-5'>
-            <GroupSubscriptionForm groupid={params.groupid} />
+            {/* <GroupSubscriptionForm groupid={params.groupid} /> */}
             {/* <Subscriptions groupid={params.groupid} /> */}
         </div>
     </div>
